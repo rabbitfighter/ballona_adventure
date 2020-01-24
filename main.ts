4eb02dc14b24ecbe900e688150b94e2287a09dca
brad <email.void@gmail.com>
2020-01-24 13:45:36 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,334 @@
+namespace SpriteKind {
+    export const sign = SpriteKind.create()
+    export const sign1 = SpriteKind.create()
+    export const sign2 = SpriteKind.create()
+    export const snowyegret = SpriteKind.create()
+    export const sign3 = SpriteKind.create()
+    export const sign4 = SpriteKind.create()
+    export const willowtree = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+e e e e e e e e e e e e e e e e 
+e e e e b e e e e e e e f e e e 
+e e e e e e e e e e e e e e e e 
+e f e e e e e e e e e e e e e e 
+e e e e e e f e e e e e b e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+b e e e e e f e e e e e f e e e 
+e e e e e f e e e e e e e e e e 
+e e e e e b e e e b e e e b e e 
+e e b e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e f e e 
+e e e e e e e e e f e e e e e e 
+e e e f e e b e e e f e e e b e 
+e e e e e e e e e e e e e e e e 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+6 6 9 9 6 6 6 6 6 6 6 6 6 6 6 6 
+6 9 9 6 6 6 6 6 6 6 6 6 6 6 9 6 
+6 6 6 9 9 9 6 6 6 9 9 9 6 6 9 9 
+6 6 6 9 6 6 6 6 9 6 6 6 9 9 9 6 
+6 6 9 9 6 6 6 9 6 6 6 9 6 6 6 6 
+6 9 9 6 6 6 9 6 6 6 9 9 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 9 9 6 
+6 6 6 6 6 6 6 6 6 6 6 6 9 9 6 6 
+6 6 6 9 9 9 6 6 6 6 6 6 9 6 6 6 
+6 6 6 9 6 9 6 6 6 6 9 9 9 6 6 6 
+6 6 9 9 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 9 6 6 6 6 9 6 6 6 6 6 6 6 6 
+6 6 6 6 6 9 9 6 6 6 6 6 6 9 9 6 
+6 6 6 6 9 9 6 6 6 6 6 9 9 9 6 6 
+9 9 6 6 6 6 6 6 6 6 9 9 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+`
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.sign1, function (sprite, otherSprite) {
+    glork.x += info1.width
+    glork.y += info1.height
+    game.showLongText("Freshwater Marsh", DialogLayout.Bottom)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.sign2, function (sprite, otherSprite) {
+    glork.x += info1.width
+    glork.y += info1.height
+    game.showLongText("Salt Pan", DialogLayout.Bottom)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.snowyegret, function (sprite, otherSprite) {
+    glork.x += info1.width
+    glork.y += info1.height
+    game.showLongText("I'm a snowy egret. I live...", DialogLayout.Bottom)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.willowtree, function (sprite, otherSprite) {
+    glork.x += info1.width
+    glork.y += info1.height
+    game.showLongText("Willow tree", DialogLayout.Bottom)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.sign3, function (sprite, otherSprite) {
+    glork.x += info1.width
+    glork.y += info1.height
+    game.showLongText("Riparian", DialogLayout.Bottom)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.sign4, function (sprite, otherSprite) {
+    glork.x += info1.width
+    glork.y += info1.height
+    game.showLongText("Upland", DialogLayout.Bottom)
+})
+let birds: Sprite = null
+let info1: Sprite = null
+let glork: Sprite = null
+glork = sprites.create(img`
+e e e . . . . e e e . . . . 
+c d d c . . c d d c . . . . 
+c b d d f f d d b c . . . . 
+c 3 b d d b d b 3 c . . . . 
+f b 3 d d d d 3 b f . . . . 
+e d d d d d d d d e . . . . 
+e d f d d d d f d e . b f b 
+f d d f d d f d d f . f d f 
+f b d d b b d d 2 f . f d f 
+. f 2 2 2 2 2 2 b b f f d f 
+. f b d d d d d d b b d b f 
+. f d d d d d b d d f f f . 
+. f d f f f d f f d f . . . 
+. f f . . f f . . f f . . . 
+`, SpriteKind.Player)
+controller.moveSprite(glork, 100, 100)
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100003030303030303030303121212141414030303030303030303031212141414140303030303030303030303121414141403030303030303030303121214141414030303030303030303121214141414140303030303030303121214141414141403030303030312121212141414141414030303030303121212121414141414140303030303031212121111141414141403030303031212121111111114141414030303031212111111111111111114140303121212111111111111111111111403121211111111111111111111111111121211111111111111111111111111111211111111111111111111111111111111111111111111111111111111111111`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.castle.tileDarkGrass1,sprites.builtin.forestTiles0,sprites.castle.tileGrass1,sprites.castle.tilePath1,sprites.castle.tilePath9,sprites.castle.tilePath2,sprites.castle.tilePath4,sprites.castle.tilePath7,sprites.castle.tilePath8,sprites.castle.tilePath6,sprites.castle.tilePath3,sprites.builtin.forestTiles8,sprites.dungeon.floorLight0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,sprites.castle.tileDarkGrass3,sprites.castle.tilePath5],
+            TileScale.Sixteen
+        ))
+tiles.placeOnTile(glork, tiles.getTileLocation(1, 1))
+scene.cameraFollowSprite(glork)
+info1 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 c c c c c c c c c c c c c c 1 
+1 c 1 c 1 1 1 c 1 1 c 1 1 1 c 1 
+1 c c c 1 c 1 c 1 c c 1 c 1 c 1 
+1 c 1 c 1 c 1 c 1 1 c 1 c 1 c 1 
+1 c 1 c 1 c 1 c 1 c c 1 c 1 c 1 
+1 c 1 c 1 c 1 c 1 c c 1 1 1 c 1 
+1 c c c c c c c c c c c c c c 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.sign1)
+tiles.placeOnTile(info1, tiles.getTileLocation(3, 3))
+let info2 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 8 8 8 8 8 8 8 8 8 8 8 8 8 8 1 
+1 8 1 8 1 1 1 8 1 1 8 1 1 1 8 1 
+1 8 8 8 1 8 1 8 1 8 8 1 8 1 8 1 
+1 8 1 8 1 8 1 8 1 1 8 1 8 1 8 1 
+1 8 1 8 1 8 1 8 1 8 8 1 8 1 8 1 
+1 8 1 8 1 8 1 8 1 8 8 1 1 1 8 1 
+1 8 8 8 8 8 8 8 8 8 8 8 8 8 8 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.sign2)
+tiles.placeOnTile(info2, tiles.getTileLocation(13, 3))
+let info3 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 c c c c c c c c c c c c c c 1 
+1 c 1 c 1 1 1 c 1 1 c 1 1 1 c 1 
+1 c c c 1 c 1 c 1 c c 1 c 1 c 1 
+1 c 1 c 1 c 1 c 1 1 c 1 c 1 c 1 
+1 c 1 c 1 c 1 c 1 c c 1 c 1 c 1 
+1 c 1 c 1 c 1 c 1 c c 1 1 1 c 1 
+1 c c c c c c c c c c c c c c 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.sign3)
+tiles.placeOnTile(info3, tiles.getTileLocation(8, 6))
+let info4 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 c c c c c c c c c c c c c c 1 
+1 c 1 c 1 1 1 c 1 1 c 1 1 1 c 1 
+1 c c c 1 c 1 c 1 c c 1 c 1 c 1 
+1 c 1 c 1 c 1 c 1 1 c 1 c 1 c 1 
+1 c 1 c 1 c 1 c 1 c c 1 c 1 c 1 
+1 c 1 c 1 c 1 c 1 c c 1 1 1 c 1 
+1 c c c c c c c c c c c c c c 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . 1 1 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.sign4)
+tiles.placeOnTile(info4, tiles.getTileLocation(8, 12))
+let snowy_egret = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . 1 1 1 . . . . . . . . 
+. . f f 1 f 1 1 . . . . . . . . 
+. . . . . 1 1 . . . . . . . . . 
+. . . . . 1 1 . 1 1 . . . . . . 
+. . . . . . 1 1 1 1 1 . . . . . 
+. . . . . . 1 1 1 1 1 1 . . . . 
+. . . . . . . 1 1 1 1 1 . . . . 
+. . . . . . . 1 1 1 1 1 1 . . . 
+. . . . . . . f 1 1 1 1 1 . . . 
+. . . . . . . f . 1 1 1 1 1 . . 
+. . . . . . . f . f 1 1 1 1 . . 
+. . . . . . . f . f . . 1 1 . . 
+. . . . . . . f . f . . . 1 . . 
+. . . . . . . f . f . . . . . . 
+. . . . . . . f . f . . . . . . 
+`, SpriteKind.snowyegret)
+tiles.placeOnTile(snowy_egret, tiles.getTileLocation(8, 8))
+let Willow = sprites.create(img`
+. . . . . . . c c . . . . . . . 
+. . . . c c c 6 5 c 6 6 . . . . 
+. . . . c 6 c 5 5 c 7 6 . . . . 
+. . . 6 c c 7 5 5 7 c 6 6 . . . 
+. . c c 7 7 7 7 7 5 7 7 c 6 . . 
+. 6 6 6 c 6 7 7 7 7 6 c c 6 6 . 
+c 7 7 7 6 c 7 c 6 7 6 7 7 7 7 6 
+c c c 6 6 6 c 6 6 6 6 7 7 6 6 6 
+. c c 7 6 6 6 6 6 7 7 7 7 c 6 . 
+. c 7 7 6 6 7 6 6 7 7 6 7 7 c . 
+. c c c c 7 7 6 f 7 7 c c c c . 
+. . . . c 7 c f f c 7 c . . . . 
+. . . . . 6 f e e e c . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . e e . e e . e e . . . . 
+. . . . . . . e e . . . . . . . 
+`, SpriteKind.willowtree)
+tiles.placeOnTile(Willow, tiles.getTileLocation(2, 10))
+game.onUpdateInterval(10000, function () {
+    birds = sprites.createProjectileFromSide(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . f f . f f . . . . . . . . . 
+. f . . f . . f . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f . f f . . . . . 
+. . . . . f . . f . . f . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. f f . f f . . . . . . . . . . 
+f . . f . . f . . . . . . . . . 
+`, 20, 40)
+})
